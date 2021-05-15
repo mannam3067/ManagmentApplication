b310c1cdf911e38409a7d4ddfa4752cb049e07f7
mallikharjuna.mannam <m.malli0005@gmail.com>
2021-05-15 22:52:52 +0530

    1.Reading value from appsettings.json, appsettings.{Environment}.json and Environment variables Command-line arguments 2.Inject the IConfiguration service in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,8 +13,15 @@ namespace ManagmentApplication
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -32,7 +40,8 @@ namespace ManagmentApplication
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                    //System.Diagnostics.Process.GetCurrentProcess().ProcessName
+                    await context.Response.WriteAsync(_config["MyKey"]);
                 });
             });
         }
