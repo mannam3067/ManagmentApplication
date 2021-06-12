6015e24906d853efeea37f5971f33a7ca97844c1
mallikharjuna.mannam <m.malli0005@gmail.com>
2021-06-12 22:35:23 +0530

    Seperated regions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ManagmentApplication.Controllers
 {
-    public class HomeController:Controller
+    public class HomeController : Controller
     {
         private IEmployeeRepository _employeeRepository;
 
@@ -12,22 +12,27 @@ namespace ManagmentApplication.Controllers
         {
             _employeeRepository = employeeRepository;
         }
-
+        #region Return Generaldatatype        
         // return employee name and return
         public string Index()
         {
             return _employeeRepository.GetEmployee(1).Name;
         }
+        // return string content 
+        public string Indexstirng()
+        {
+            return "Hello MVC";
+        }
+        #endregion
+
+        #region ReturnMvcActionResults
+
+
         // return employee details using employee id
         public ViewResult Details(int id)
         {
             Employee employee = _employeeRepository.GetEmployee(id);
             return View();
-        }
-        // return string content 
-        public string Indexstirng()
-        {
-            return "Hello MVC";
         }
 
         // return employee details using Json return type
@@ -35,5 +40,7 @@ namespace ManagmentApplication.Controllers
         {
             return Json(new { id = 1, Name = "mallikharjuna" });
         }
+        #endregion
+
     }
 }
