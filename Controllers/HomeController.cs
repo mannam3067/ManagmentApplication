c212021db6fa0f236696e31de8cb2128aa008b1e
mallikharjuna.mannam <m.malli0005@gmail.com>
2021-06-12 22:33:53 +0530

    1.Created Home controller with diferrent return types 2.Created view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ManagmentApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagmentApplication.Controllers
+{
+    public class HomeController:Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // return employee name and return
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        // return employee details using employee id
+        public ViewResult Details(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            return View();
+        }
+        // return string content 
+        public string Indexstirng()
+        {
+            return "Hello MVC";
+        }
+
+        // return employee details using Json return type
+        public JsonResult IndexJson()
+        {
+            return Json(new { id = 1, Name = "mallikharjuna" });
+        }
+    }
+}
